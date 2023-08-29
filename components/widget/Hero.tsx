1a9b3e4099b63bd262bfb5cc0d9f8c1600a3a0a0
IsraelChidera <israelchidera54@gmail.com>
2023-08-29 22:30:03 +0100

    Feature: more stylings on the Hero component

--- components/widget/Hero.tsx
+++ components/widget/Hero.tsx
@@ -5,7 +5,7 @@ import line from '../../public/assets/line.svg';
 
 const Hero = () => {
     return (
-        <header className='pt-4 mx-auto md:w-5/6'>
+        <header className='mx-auto md:w-5/6'>
             <div className='flex justify-between'>
                 <section className='flex items-center'>
                     <div className='space-y-4'>
@@ -13,7 +13,7 @@ const Hero = () => {
                             <span>Your health is</span> <br />
                             <span className='text-primary'>our priority</span>
 
-                            <Image src={line} alt="line icon" className='absolute top-5 right-0'/>
+                            <Image src={line} alt="line icon" className='absolute top-5 right-0' />
                         </div>
 
                         <p className='text-xl w-3/4'>
@@ -27,6 +27,44 @@ const Hero = () => {
 
                 <div>
                     <Image src={hero} alt="hero image" className='hero-image' />
+                </div>
+            </div>
+
+            <div className='md:grid grid-cols-3 gap-x-6 gap-y-6 mt-14 mb-6'>
+                <div className='rectangle'>
+                    <h3 className='font-bold text-xl'>
+                        Consult a professional
+                    </h3>
+
+                    <p className='pt-2'>
+                        Book an appointment with
+                        an expert and be attended
+                        to in minutes.
+                    </p>
+                </div>
+
+                <div className='rectangle'>
+                    <h3 className='font-bold text-xl'>
+                        Upload your prescription
+                    </h3>
+
+                    <p className='pt-2'>
+                        Have a prescription from a doctor?
+                        Upload it now and we will
+                        deliver it to you.
+                    </p>
+                </div>
+
+                <div className='rectangle'>
+                    <h3 className='font-bold text-xl'>
+                        Purchase your medication
+                    </h3>
+
+                    <p className='pt-2'>
+                        Place an order for your medication 
+                        and it will be available 
+                        in no time.
+                    </p>
                 </div>
             </div>
         </header>
