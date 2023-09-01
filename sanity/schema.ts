afade54e8b35161cb9e92c1464a9c2fe58fbbdf9
IsraelChidera <israelchidera54@gmail.com>
2023-09-01 22:24:41 +0100

    Setting up Sanity studio

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,6 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from 'sanity';
+import product from './schemas/product';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [product],
 }
