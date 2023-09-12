2a7c1f8931250205d6737599900a2c090a8f7974
IsraelChidera <israelchidera54@gmail.com>
2023-09-12 12:41:20 +0100

    COmpleted checkout page

--- app/cart/checkout/page.tsx
+++ app/cart/checkout/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react';
+import React, { useState } from 'react';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -13,6 +13,18 @@ import product1 from '../../../public/assets/product.png';
 import Image from "next/image";
 
 const page = () => {
+
+    const [selectedOption, setSelectedOption] = useState('Store pickup');
+    const [selectedPaymentOption, setSelectedPaymentOption] = useState("Debit/credit card");
+
+    const handleChange = (event: any) => {
+        setSelectedOption(event.target.value);
+    }
+
+    const handlePaymentMethodChange = (event: any) => {
+        setSelectedPaymentOption(event.target.value);
+    }
+
     return (
         <section className='md:mt-0 grid grid-cols-1 mt-10 md:grid px-3 md:px-0 md:grid-cols-2 md:space-x-20 mx-auto md:w-5/6 md:py-8 py-10'>
             <div className='md:order-1 order-2'>
@@ -36,9 +48,12 @@ const page = () => {
                                         fontSize: 20,
                                     },
                                 }}
+                                defaultValue={selectedOption}
+                                // value={selectedOption}
+                                onChange={handleChange}
                             >
                                 <FormControlLabel
-                                    value={<span>Store pickup</span>}
+                                    value={"Store pickup"}
                                     control={<Radio />}
                                     label={<span className='text-sm'>Store pickup</span>}
                                 />
@@ -50,19 +65,98 @@ const page = () => {
                                 />
                             </RadioGroup>
                         </FormControl>
+
                     </div>
 
-                    <div className='space-y-2 mt-4'>
-                        <p className="text-xs"> To pickup directly from the store, visit the following address: </p>
+                    {selectedOption === 'Store pickup'
+                        && <div className='space-y-2 mt-4'>
+                            <p className="text-xs"> To pickup directly from the store, visit the following address: </p>
 
-                        <div className='text-sm font-bold'>
-                            <p>PharmaBolt Pharmaceuticals</p>
-                            <p> 15, Park Lane Avenue, G.R.A</p>
-                            <p>Enugu, Enugu state</p>
+                            <div className='text-sm font-bold'>
+                                <p>PharmaBolt Pharmaceuticals</p>
+                                <p> 15, Park Lane Avenue, G.R.A</p>
+                                <p>Enugu, Enugu state</p>
+                            </div>
+
+                            <p className='text-xs'> Please come with a valid reciept of your payment or transaction ID during pickup. </p>
                         </div>
+                    }
 
-                        <p className='text-xs'> Please come with a valid reciept of your payment or transaction ID during pickup. </p>
-                    </div>
+                    {selectedOption === 'Home delivery' &&
+                        <Box
+                            component="form"
+                            className='mt-6 space-y-6'
+                            noValidate
+                            autoComplete="off"
+                        >
+                            <div>
+                                <TextField
+                                    required
+                                    id="outlined-required"
+                                    label={<span className='text-sm'>Full Name</span>}
+                                    size="small"
+                                    className='w-full'
+                                />
+                            </div>
+
+                            <div>
+                                <TextField
+                                    required
+                                    type="email"
+                                    id="outlined-required"
+                                    label={<span className='text-sm'>Email address</span>}
+                                    size="small"
+                                    className='w-full'
+                                />
+                            </div>
+
+                            <div>
+                                <TextField
+                                    required
+                                    type="tel"
+                                    id="outlined-required"
+                                    label={<span className='text-sm'>Phone number</span>}
+                                    size="small"
+                                    className='w-full'
+                                />
+                            </div>
+
+                            <div>
+                                <TextField
+                                    type="text"
+                                    required
+                                    id="outlined-required"
+                                    label={<span className='text-sm'>Delivery address</span>}
+                                    size="small"
+                                    className='w-full'
+                                />
+                            </div>
+
+                            <div>
+                                <TextField
+                                    required
+                                    type="text"
+                                    id="outlined-required"
+                                    label={<span className='text-sm'>City </span>}
+                                    size="small"
+                                    className='w-full'
+                                />
+                            </div>
+
+                            <div>
+                                <TextField
+                                    required
+                                    id="outlined-required"
+                                    type="text"
+                                    label={<span className='text-sm'> State </span>}
+                                    size="small"
+                                    className='w-full'
+                                />
+                            </div>
+                        </Box>
+                    }
+
+
                 </div>
 
                 <div className='mt-10'>
@@ -82,9 +176,11 @@ const page = () => {
                                     fontSize: 20,
                                 },
                             }}
+                            defaultValue={selectedPaymentOption}
+                            onChange={handlePaymentMethodChange}
                         >
                             <FormControlLabel
-                                value={<span>Debit/credit card</span>}
+                                value="Debit/credit card"
                                 control={<Radio />}
                                 label={<span className='text-sm'>Debit/credit card</span>}
                             />
@@ -96,35 +192,15 @@ const page = () => {
                         </RadioGroup>
                     </FormControl>
 
-                    <div className='mt-6'>
-                        <Box
-                            component="form"
-                            className='space-y-6'
-                            noValidate
-                            autoComplete="off"
-                        >
-                            <div>
-                                <TextField
-                                    required
-                                    id="outlined-required"
-                                    label={<span className='text-sm'>Card number</span>}
-                                    size="small"
-                                    className='w-full'
-                                />
-                            </div>
-
-                            <div className='md:grid grid-cols-2 md:gap-x-2 space-y-4'>
-                                <div>
-                                    <TextField
-                                        required
-                                        id="outlined-password-input"
-                                        label="Password"
-                                        type="password"
-                                        size="small"
-                                        className='w-full'
-                                    />
-                                </div>
-
+                    {
+                        selectedPaymentOption === 'Debit/credit card' &&
+                        <div className='mt-6'>
+                            <Box
+                                component="form"
+                                className='space-y-6'
+                                noValidate
+                                autoComplete="off"
+                            >
                                 <div>
                                     <TextField
                                         required
@@ -134,25 +210,75 @@ const page = () => {
                                         className='w-full'
                                     />
                                 </div>
-                            </div>
 
-                            <div>
-                                <TextField
-                                    required
-                                    id="outlined-password-input"
-                                    label="email"
-                                    type="email"
-                                    size="small"
-                                    className='w-full'
-                                />
-                                <p className='text-xs'>
-                                    Your transaction reciept will be sent to this email*
+                                <div className='md:grid grid-cols-2 md:gap-x-2 '>
+                                    <div>
+                                        <TextField
+                                            required
+                                            id="outlined-password-input"
+                                            label="Password"
+                                            type="password"
+                                            size="small"
+                                            className='w-full'
+                                        />
+                                    </div>
+
+                                    <div>
+                                        <TextField
+                                            required
+                                            id="outlined-required"
+                                            label={<span className='text-sm'>Card number</span>}
+                                            size="small"
+                                            className='w-full'
+                                        />
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <TextField
+                                        required
+                                        id="outlined-password-input"
+                                        label="email"
+                                        type="email"
+                                        size="small"
+                                        className='w-full'
+                                    />
+                                    <p className='text-xs'>
+                                        Your transaction reciept will be sent to this email*
+                                    </p>
+                                </div>
+                            </Box>
+
+                            <CustomButton title="Proceed to summary" className='py-3 w-full mt-8' />
+                        </div>
+                    }
+
+                    {
+                        selectedPaymentOption === 'Bank transfer' &&
+                        <div>
+                            <div className='space-y-2 mt-4'>
+                                <p className="text-xs"> Make your payment directly into the bank account: </p>
+
+                                <div className='text-sm font-bold'>
+                                    <p>PharmaBolt Pharmaceuticals</p>
+                                    <p> GT Bank </p>
+                                    <p>Account number: 0983452671</p>
+                                </div>
+
+                                <p className='text-sm'>
+                                    Please use your transaction ID as a payment reference.
+                                    Your transaction ID will be generated once you
+                                    select "Bank Transfer" and click on "Checkout".
+                                </p>
+
+                                <p className='pt-4 text-sm'>
+                                    Your order will only be shipped once payment is confirmed.
                                 </p>
                             </div>
-                        </Box>
 
-                        <CustomButton title="Proceed to summary" className='py-3 w-full mt-8' />
-                    </div>
+                            <CustomButton title="Proceed to summary" className='py-3 w-full mt-8' />
+                        </div>
+                    }
                 </div>
             </div>
 
